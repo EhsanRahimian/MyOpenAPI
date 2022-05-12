3e94929919059ca6abddd28742d80207d2d27a42
Ethan <nicootech@gmail.com>
2022-05-12 15:25:27 -0700

    4-Room Persistence Setup (#3)

--- app/src/main/java/com/nicootech/myopenapi/persistence/AppDatabase.kt
+++ app/src/main/java/com/nicootech/myopenapi/persistence/AppDatabase.kt
@@ -1,5 +1,20 @@
 package com.nicootech.myopenapi.persistence
 
-annotation class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.nicootech.myopenapi.models.AccountProperties
+import com.nicootech.myopenapi.models.AuthToken
+
+@Database(entities = [AuthToken::class, AccountProperties::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+
+    companion object{
+        const val DATABASE_NAME: String = "app_db"
+    }
+
 
 }
