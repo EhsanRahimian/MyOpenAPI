bfa003d17f745c54f4a494b86e3581660b85d265
Ethan Rahimian <nicootech@gmail.com>
2022-05-12 15:24:47 -0700

    4-Room Persistence Setup

--- app/src/main/java/com/nicootech/myopenapi/persistence/AuthTokenDao.kt
+++ app/src/main/java/com/nicootech/myopenapi/persistence/AuthTokenDao.kt
@@ -1,5 +1,16 @@
 package com.nicootech.myopenapi.persistence
 
-interface AuthTokenDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.nicootech.myopenapi.models.AuthToken
 
+@Dao
+interface AuthTokenDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(authToken: AuthToken): Long
+
+    @Query("UPDATE auth_token SET token = null WHERE account_pk = :pk")
+    fun nullifyToken(pk: Int): Int
 }
