669ea27085a67669be3cd9799aa8e75c6618991c
Ethan <nicootech@gmail.com>
2022-05-12 19:25:59 -0700

    6-Retrofit Setup (#5)

--- app/src/main/java/com/nicootech/myopenapi/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/nicootech/myopenapi/ui/auth/AuthViewModel.kt
@@ -1,7 +1,11 @@
 package com.nicootech.myopenapi.ui.auth
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.nicootech.myopenapi.api.auth.network_responses.LoginResponse
+import com.nicootech.myopenapi.api.auth.network_responses.RegistrationResponse
 import com.nicootech.myopenapi.repository.auth.AuthRepository
+import com.nicootech.myopenapi.util.GenericApiResponse
 import javax.inject.Inject
 
 class AuthViewModel
@@ -9,4 +13,19 @@ class AuthViewModel
 constructor(val authRepository: AuthRepository): ViewModel()
 {
 
+    fun testLogin(): LiveData<GenericApiResponse<LoginResponse>> {
+        return authRepository.testLoginRequest(
+            "mitchelltabian@gmail.com",
+            "codingwithmitch1"
+        )
+    }
+
+    fun testRegister(): LiveData<GenericApiResponse<RegistrationResponse>> {
+        return authRepository.testRegistrationRequest(
+            "mitchelltabian1234@gmail.com",
+            "mitchelltabian1234",
+            "codingwithmitch1",
+            "codingwithmitch1"
+        )
+    }
 }
